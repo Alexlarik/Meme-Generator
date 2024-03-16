30d1e88a7a45d52dcaddde9a94a1b0cbf0102119
alexl <alexlarik13@gmail.com>
2024-03-16 19:43:16 +0200

    Orginize the images and add a canvas with a bit of styling

--- js/main.controller.js
+++ js/main.controller.js
@@ -1,15 +1,23 @@
 'use strict'
 
-var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
-var gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'I sometimes eat Falafel',
-            size: 20,
-            color: 'red'
-        }
-    ]
+// var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+// var gMeme = {
+//     selectedImgId: 5,
+//     selectedLineIdx: 0,
+//     lines: [
+//         {
+//             txt: 'I sometimes eat Falafel',
+//             size: 20,
+//             color: 'red'
+//         }
+//     ]
+// }
+// var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+
+function drawImg(meme) {
+    const elImg = meme
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+    drawText(prompt(), gElCanvas.width /2, gElCanvas.height / 5)
 }
-var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 'use strict'
 
+let gElCanvas
+let gCtx
+
 function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+}
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 3
+    gCtx.strokeStyle = 'black'
+
+    gCtx.font = '20px Arial'
+    gCtx.textAlign = 'center';
+    gCtx.textBaseline = 'center';
+
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
 
 }
